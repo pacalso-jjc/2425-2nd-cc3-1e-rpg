84689c340fa25aed61eaa741182e4b9614433e23
denioeb1 <ebd4722@students.uc-bcf.edu.ph>
2025-03-23 20:29:33 +0800

    Update DialogueManager.java

--- wiki/DialogueManager.java
+++ wiki/DialogueManager.java
@@ -8,16 +8,7 @@
     public void addDialogue(Dialogue dialogue) {
         dialogues.add(dialogue);
     }
-
-    public Dialogue getDialogue(String text) {
-        for (Dialogue dialogue : dialogues) {
-            if (dialogue.getText().equals(text)) {
-                return dialogue;
-            }
-        }
-        return null;
-    }
-
+      
     public void startDialogue(Dialogue dialogue) {
         System.out.println("Starting dialogue: " + dialogue.getText());
     }
