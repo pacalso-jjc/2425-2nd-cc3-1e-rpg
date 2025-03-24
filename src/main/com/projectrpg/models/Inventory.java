94d7369f08cd0f1d71d807e405c071b167dd731c
aoxxoa <a.a.oxxo.a@gmail.com>
2025-03-24 18:18:02 +0900

    Update: inventory code changed

--- src/main/com/projectrpg/models/Inventory.java
+++ src/main/com/projectrpg/models/Inventory.java
@@ -1,73 +1,30 @@
-public class Inventory {
+import java.util.ArrayList;
+import java.util.List;
 
-    private int itemID;
-    private String itemName;
-    private int quantity;
-    private float price;
+class Inventory {
+    private List<Item> items;
 
-    public Inventory(int itemID, String itemName, int quantity, float price) {
-        this.itemID = itemID;
-        this.itemName = itemName;
-        this.quantity = quantity;
-        this.price = price;
+    public Inventory() {
+        this.items = new ArrayList<>();
     }
 
-    public int getItemID() {
-        return itemID;
+    public void addItem(Item item) {
+        items.add(item);
+        System.out.println("Added item: " + item.getName());
     }
 
-    public void setItemID(int itemID) {
-        this.itemID = itemID;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public void addItem(int quantity) {
-        if (quantity > 0) {
-            this.quantity += quantity;
-            System.out.println(quantity + " items added to inventory.");
+    public void removeItem(Item item) {
+        if (items.remove(item)) {
+            System.out.println("Removed item: " + item.getName());
         } else {
-            System.out.println("Invalid quantity to add.");
-        }
-    }
-
-    public void removeItem(int quantity) {
-        if (quantity > 0 && this.quantity >= quantity) {
-            this.quantity -= quantity;
-            System.out.println(quantity + " items removed from inventory.");
-        } else if (quantity > 0) {
-            System.out.println("Insufficient items in inventory.");
-        } else {
-            System.out.println("Invalid quantity to remove.");
+            System.out.println("Item not found: " + item.getName());
         }
     }
 
     public void displayInventory() {
-        System.out.println("Item ID: " + itemID);
-        System.out.println("Item Name: " + itemName);
-        System.out.println("Quantity: " + quantity);
-        System.out.println("Price: $" + price);
+        System.out.println("Inventory:");
+        for (Item item : items) {
+            System.out.println("- " + item.getName() + " (Type: " + item.getType() + ", Weight: " + item.getWeight() + ", Value: " + item.getValue() + ")");
+        }
     }
 }
