c5557df08611c6ca348544d1c218be3278fa2123
Cherry Ann Doyaoen <cbd7242@students.uc-bcf.edu.ph>
2025-04-02 17:00:56 +0800

    Update and rename Spearman.java to src/Spearman.java

--- src/Spearman.java
+++ src/Spearman.java
@@ -1,21 +1,21 @@
 // Derived class representing a Spearman, inheriting from Character
 class Spearman extends Character {
     public int spearSkillLevel; 
-    private Spear equippedSpear; 
+    private Weapon equippedSpear; 
 
-    public void Attack() {
+    public void attack() {
         System.out.println("The Spearman attacks with a spear!");
     }
 
-    public void Block() {
+    public void block() {
         System.out.println("The Spearman blocks the incoming attack!");
     }
 
-    public void Counter() {
+    public void counter() {
         System.out.println("The Spearman counters the attack!");
     }
 
-    public void Dodge() {
+    public void dodge() {
         System.out.println("The Spearman dodges the attack!");
     }
 }
