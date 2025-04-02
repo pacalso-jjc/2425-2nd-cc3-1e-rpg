c01e0ccac9a82990c0a72881ea2a3215e50733bb
Mark Alexon Miranda <mgm4503@students.uc-bcf.edu.ph>
2025-04-02 17:18:24 +0800

    Settings

---
+++ src/main/java/com/projectrpg/Settings.java
@@ -0,0 +1,19 @@
+public class Setting {
+
+    private int textDelay;
+
+    // Constructor
+    public Setting(int textDelay) {
+        this.textDelay = textDelay;
+    }
+
+    // Getter for textDelay
+    public int getTextDelay() {
+        return textDelay;
+    }
+
+    // Setter for textDelay
+    public void setTextDelay(int textDelay) {
+        this.textDelay = textDelay;
+    }
+}
