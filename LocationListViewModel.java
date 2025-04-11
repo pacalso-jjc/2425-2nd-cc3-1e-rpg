a752cf24803bbac1622df739818a012474feb7c6
Glexainth John D. Barcarse <gdb3517@students.uc-bcf.edu.ph>
2025-04-11 23:38:34 +0800

    Create LocationListViewModel.java
    Add basic location selection setup 
    
    - Created LocationListViewModel to manage:
      - List of locations (string array) 
      - Currently selected location (selectedIndex) 
      - Basic selection functionality via select() method
      
    - Empty load() method added (TODO for later) 
    - Simple structure for tracking user's location choice
    
    Next steps: Connect to actual locations data and UI. Please check sir @pacalso-jjc . Thank you !

---
+++ LocationListViewModel.java
@@ -0,0 +1,12 @@
+public class LocationListViewModel {
+    private String[] locations;
+    private int selectedIndex;
+
+    public void load() {
+        // Implementation for loading locations
+    }
+
+    public void select(int index) {
+        this.selectedIndex = index;
+    }
+}
